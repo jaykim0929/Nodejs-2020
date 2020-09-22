4d36819999ebc4e2ff7025c2ee674599fd1e18a1
jaykim0929 <killguga@naver.com>
2020-09-22 14:45:50 +0900

    반복문

--- 04.반복문/01.array.js
+++ 04.반복문/01.array.js
@@ -1,22 +1,23 @@
-let array =[52, 273.1, '아침밥', '점심밥', true, false]
-console.log(array.length);
-console.log(array);
+let array = [52, 273.1, '아침밥', '점심밥', true, false];
+//console.log(array.length);
+//console.log(array);
 
-console.log(array[2], array[3]);
+//console.log(array[2], array[3]);
 array[0] = 0;
-console.log(array);
+//console.log(array);
 
 // element 추가
 array[6] = 'new item';
-console.log(array);
+//console.log(array);
 array.push('push');
-console.log(array);
+//console.log(array);
 
 for (let i=0; i<array.length; i++) {
     console.log(array[i], typeof(array[i]));
 }
 console.log();
-// Enhaced For Loop
+
+// Enhanced For Loop
 for (let item of array) {
     console.log(item, typeof(item));
 }
