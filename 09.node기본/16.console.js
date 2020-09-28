35bfd6f788ab4d5198113272e481906243e81e1d
jaykim0929 <killguga@naver.com>
2020-09-28 18:17:24 +0900

    node 기본

--- 09.node기본/16.console.js
+++ 09.node기본/16.console.js
@@ -1,10 +1,14 @@
 //시간 측정을 시작 합니다
 console.time('alpha');
 
-var output =1;
-for (bar i = 1; i <= 10; i++) {
+/* var output =1;
+for (var i = 1; i <= 10; i++) {
     output *= i;
 
+}  */
+let sum = 0;
+for (let i=1; i<=1e6; i++) {
+    sum += i;
 }
 console.log('Result:', output);
 
